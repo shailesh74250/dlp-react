9aaff66e52e81d903f12f79ae9a32513e6ffbf45
shailesh74250 <smourya7425@gmail.com>
2020-06-30 10:07:59 +0530

    Remove route, switch from routing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import Navbar from '../layouts/navbar';
 import Footer from '../layouts/footer';
-import Home from '../features/landing_page';
+import LandingPage from '../features/landing_page';
 import Loader from '../components/Loader';
 import MoveTop from '../components/Buttons/MoveTop';
 
@@ -10,12 +10,8 @@ function Routing() {
   return (
     <Router>
       <Loader />
-      <Navbar />  
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Navbar />
+      <LandingPage />
       <Footer />
       <MoveTop />
     </Router>
